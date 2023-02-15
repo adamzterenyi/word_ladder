184b9b83c42eca20588822f7f8adb33fb027aac4
adamzterenyi <adamzterenyi@gmail.com>
2023-02-14 22:24:32 -0800

    fixed linter issue

--- word_ladder.py
+++ word_ladder.py
@@ -65,8 +65,8 @@ def verify_word_ladder(ladder):
     if ladder is None or len(ladder) == 0:
         return False
     for i in range(len(ladder) - 1):
-            if not _adjacent(ladder[i], ladder[i + 1]):
-                return False
+        if not _adjacent(ladder[i], ladder[i + 1]):
+            return False
     return True
 
 
